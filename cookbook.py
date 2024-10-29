c4fcc8a7b3592b74a9bf7c7172dea922e1e4b3c6
VKo65 <volker@kolauch.de>
2024-10-29 13:19:26 +0100

    The 1.0 version of cookbook

---
+++ cookbook.py
@@ -0,0 +1,27 @@
+import json
+
+def adding_recipes():
+    """Recipe Adding Functionality - Create a function that
+    takes user input for a recipe name, ingredients, and instructions and
+    saves it to recipes.json."""
+   pass
+
+def listing_recipes():
+    """Listing Recipes - Implement functionality
+     to list all recipes in recipes.json with their names and a brief description. """
+    pass
+
+def search_recipes():
+    """Search by Recipe Name - Build a search feature allowing users to find a recipe by name."""
+    pass
+
+def display_recipes():
+    """Display Recipe Details - Create a function to display
+    the full details (ingredients and instructions) of a specific recipe."""
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
