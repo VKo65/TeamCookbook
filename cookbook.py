e8318a45a6431471154ffaf46449f1a2e036aae1
kayeffemm <kenneth.meier95@gmail.com>
2024-10-31 10:22:21 +0100

    Refactor project structure
    Refactored the whole Project structure.
    Added comments with assigned Team Member.

--- cookbook.py
+++ cookbook.py
@@ -1,27 +1,44 @@
-import json
+from cookbook_storage import get_cookbook_data, write_cookbook_data
 
+# assigned member: Chris
 def adding_recipes():
-    """Recipe Adding Functionality - Create a function that
+    """
+    Recipe Adding Functionality - Create a function that
     takes user input for a recipe name, ingredients, and instructions and
-    saves it to recipes.json."""
-   pass
-
-def listing_recipes():
-    """Listing Recipes - Implement functionality
-     to list all recipes in recipes.json with their names and a brief description. """
+    parses it to write_cookbook_data().
+    :return:
+    """
     pass
 
-def search_recipes():
-    """Search by Recipe Name - Build a search feature allowing users to find a recipe by name."""
+# assigned member: Felipe
+def list_recipes() -> None:
+    """
+    Uses get_cookbook_data() to get all data and prints out every entry by name and a short description.
+    :return: None
+    """
     pass
 
-def display_recipes():
-    """Display Recipe Details - Create a function to display
-    the full details (ingredients and instructions) of a specific recipe."""
+# assigned member: Suti
+def search_recipes(cookbook_data: list) -> int:
+    """
+    Prompts User to enter a search term, and searches for matches in given list.
+    If found returns it's Index
+    :return: int
+    """
     pass
+
+# assigned member: Kenneth
+def display_recipes() -> None:
+    """
+    Calls search_recipes() to get index of desired recipe and prints out all details.
+    :return: None
+    """
+    pass
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
---
+++ cookbook_storage.py
@@ -0,0 +1,47 @@
+import json
+
+FILEPATH = "cookbook_database.json"
+
+# assigned member: Felipe
+def get_cookbook_data() -> list:
+    """
+    Opens the JSON file and returns data as python datastructure.
+    :return: list
+    """
+    pass
+
+# assigned member: Chris
+def write_cookbook_data(new_data: list) -> None:
+    """
+    Writes given data into the JSON file.
+    :param new_data: list
+    :return: None
+    """
+    pass
+
+# following three functions are not needed if we provide a JSON with default data.
+def write_default_data() -> None:
+    """
+    Creates JSON file with default data.
+    :return:
+    """
+    pass
+
+
+def validate_existence() -> bool:
+    """
+    Checks if the file exists, if data
+    does not exist function calls write_default_data().
+    :return: Bool
+    """
+    pass
+
+
+def validate_data() -> bool:
+    """
+    Calls validate_existence() to check if there is any data,
+    if there is data to work with it reads the data and checks
+    if data is valid. In case data is invalid it calls write_default_data().
+    :returns: Boolean
+    """
+    pass
